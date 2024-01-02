5d73050b661f6725b2387934249d5439fea6e77f
Wictor-Hugo-S <wictorhugo928@gmail.com>
2024-01-02 20:04:53 -0300

    add

--- src/Pages/SobreMim/listaFormacao.js
+++ src/Pages/SobreMim/listaFormacao.js
@@ -7,7 +7,7 @@ const listaFormacao=[
         id:0,
         instituicao:'Wyden UniFBV',
         curso:'Ánalise e Desenvolvimento de Sistemas' ,
-        descricao:' 1º Período ',
+        descricao:' 2º Período ',
         modalidade:'EAD-Online',
         situacao:'Cursando',
         ano:'Junho 2023 - Dezembro 2025 '
